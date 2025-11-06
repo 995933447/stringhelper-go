e7fcf480d31691e9177dc6303b6fc35b14c59810
995933447 <34621597+995933447@users.noreply.github.com>
2025-11-06 15:50:56 +0100

    Add files via upload

--- test/fmt_test.go
+++ test/fmt_test.go
@@ -1,10 +1,12 @@
 package test
 
 import (
-	"github.com/995933447/stringhelper-go"
 	"testing"
+
+	"github.com/995933447/stringhelper-go"
 )
 
-func TestCamel(t *testing.T) {
+func TestFmt(t *testing.T) {
 	t.Log(stringhelper.Camel("Ab_cd_ef"))
+	t.Log(stringhelper.Snake("Ab_cd_ef"))
 }
